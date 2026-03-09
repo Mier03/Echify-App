0e7fc435b80a5be3ec043d6c59b608e992ee36e4
Angelina Mier <mierangelina03@gmail.com>
2026-03-09 19:36:34 +0800

    Revert "Update start_up.sh"
    This reverts commit 57aeb24a476ba47669483a5ae323a0918db80909.

--- start_up.sh
+++ start_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BASE_DIR="/home/sms/Echify-App"
-MODEL_PATH="$BASE_DIR/backend/models/lstm_static/best_fsl_lstm_model.pth"
+MODEL_PATH="$BASE_DIR/backend/models"
 
 echo "🔄 Updating project from GitHub..."
 cd "$BASE_DIR" || exit 1
