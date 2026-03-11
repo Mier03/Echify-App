e80a9bf042faf0e7e710c22cacb35dcff23bf191
Rainelyn Sungahid <rainelynsungahid@gmail.com>
2026-03-11 17:45:45 +0800

    180degrees flip camera

--- components/CameraView.tsx
+++ components/CameraView.tsx
@@ -70,6 +70,8 @@ const styles: any = StyleSheet.create({
     height: "100%",
     objectFit: "cover",
     display: "block",
+    transform: [{ rotate: '180deg' }], 
+    WebkitTransform: 'rotate(180deg)',
   },
   loadingOverlay: {
     position: "absolute",
