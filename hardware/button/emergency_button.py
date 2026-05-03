0e5823117591d01b4fd822222c790b8b837c3566
Mitch Lauren A. Santillan <santillanmitchlauren@gmail.com>
2026-05-04 00:33:36 +0800

    Revert "file path"
    This reverts commit feaf3b920a95c9ae89450c202e05c3171840a0cd.

--- hardware/button/emergency_button.py
+++ hardware/button/emergency_button.py
@@ -4,7 +4,6 @@ from tts.tts_engine import EmergencyAudio
 import time
 from signal import pause
 import requests
-import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../backend")))
